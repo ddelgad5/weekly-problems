ac15ece843a8f8c2c02db15796746a1e3b42b728
David Delgado <ddelgad5@hawk.iit.edu>
2018-06-04 21:06:48 -0500

    Add API call to Divvy.

--- week-02/index.js
+++ week-02/index.js
@@ -15,7 +15,7 @@ const options = {
   }
 };
 
-req(options)
+req(options) // Github API Call
   .then(
     // TODO: Handle the returned JSON data and write it to a file called
     // `response.json` in your `week-two/` directory
@@ -25,7 +25,34 @@ req(options)
           throw err;
         }
       });
-      console.log('Request successful!');
+      console.log('Github request successful!');
+    }
+  )
+  .catch(function(err) {
+    console.log(err);
+  });
+
+const dOptions = {
+  // TODO: Create an object literal to hit your account on the GitHub API,
+  // and pass any other information the API requires (you should not have
+  // to authenticate for this, however)
+  url:"https://feeds.divvybikes.com/stations/stations.json",
+  headers:  {
+    'User-Agent': 'Request-Promise'
+  }
+};
+
+req(dOptions) // Divvy API call
+  .then(
+    // TODO: Handle the returned JSON data and write it to a file called
+    // `response.json` in your `week-two/` directory
+    function(results) {
+      fs.writeFile('divvy.json', results, (err) => {
+        if (err) {
+          throw err;
+        }
+      });
+      console.log('Divvy request successful!');
     }
   )
   .catch(function(err) {
