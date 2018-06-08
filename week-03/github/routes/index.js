a538a99f106990cb0c96741164235bbbd07c4edc
David Delgado <ddelgad5@hawk.iit.edu>
2018-06-07 21:12:42 -0500

    Fetch Github data, parse, and send data to template.

--- week-03/github/routes/index.js
+++ week-03/github/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var rpn = require('request-promise-native');
+
+const options = {
+  url:"https://api.github.com/users/ddelgad5",
+  headers:  {
+    'User-Agent': 'Request-Promise'
+  },
+  json: true
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  rpn(options).then( function(results) {
+    console.log("Github profile", results.login, "called");
+    res.render('index', { title: 'Github', name: results.name, avatar: results.avatar_url, repos: results.public_repos, updated_at: Date(results.updated_at) });
+  });
 });
 
 module.exports = router;
